5ee1033b364dcca43be62c649153eed23e962b43
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-10 19:54:08 -0500

    Create method to change state

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count: </Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter} </Text>
     </View>
   );
 };
