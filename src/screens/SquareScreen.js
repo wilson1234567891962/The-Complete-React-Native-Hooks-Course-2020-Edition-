dd9790a96508b45d3e9875beb998c588be584a4d
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-10 21:25:24 -0500

    Create method to valide color

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,29 +2,47 @@ import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ColorCounter from "../ComponentsScreen/ColorCounter";
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
-  console.log(red);
+  const setColor = (color, change) => {
+    switch (color) {
+      case "red":
+        red + change > 255 || red + change < 0 ? null : setRed(red + change);
+        return;
+      case "green":
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change);
+        return;
+      case "blue":
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+        return;
+      default:
+        return;
+    }
+  };
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setColor("red", COLOR_INCREMENT)}
+        onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setGreen(green + 1)}
-        onDecrease={() => setGreen(green - 1)}
-        color="Blue"
+        onIncrease={() => setColor('green', COLOR_INCREMENT)}
+        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+        color="Green"
       />
       <ColorCounter
-        onIncrease={() => setBlue(blue + 1)}
-        onDecrease={() => setBlue(blue - 1)}
-        color="Green"
+        onIncrease={() => setColor('blue' ,  COLOR_INCREMENT)}
+        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+        color="Blue"
       />
       <View
         style={{
