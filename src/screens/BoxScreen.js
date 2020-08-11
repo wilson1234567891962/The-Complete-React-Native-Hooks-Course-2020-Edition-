1d1a6d5bea0f87646da7adb12a76d2382ec1650c
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-11 13:24:22 -0500

    Exercise to locate view

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,25 +1,45 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Button, Text } from "react-native";
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-    return (
-      <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>Current Count </Text>
+  return (
+    <View style={styles.parentStyle}>
+      <View style={styles.viewOneStyle} />
+      <View style={styles.viewTwoParent}>
+        <View style={styles.viewTwoStyle} />
       </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-      viewStyle: {
-          borderWidth: 3,
-          borderColor: 'black'
-      },
-      textStyle: {
-        borderWidth: 2,
-        borderColor: 'red',
-        margin: 20
-      }
-  });
-  
-  export default BoxScreen;
-  
+      <View style={styles.viewThreeStyle} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  parentStyle: {
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
+  },
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green'
+  },
+  viewTwoParent: {
+    height: 100,
+    justifyContent: 'flex-end'
+  },
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple'
+  }
+});
+
+export default BoxScreen;
